89769be94b1a226b6527a4afb1bf6b614027812e
Monish Yeluguri <yeluguri.monish@gmail.com>
2021-11-06 16:48:19 -0400

    added routing

---
+++ src/components/drug-data/DrugData.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Divider } from "semantic-ui-react";
+
+export default class DrugData extends Component {
+  render() {
+    return <div>hello</div>;
+  }
+}
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,30 +5,11 @@ import { Dropdown } from "semantic-ui-react";
 
 import "./Header.css";
 
-const countryOptions = [
-  { key: "af", value: "af", flag: "af", text: "Afghanistan" },
-  { key: "ax", value: "ax", flag: "ax", text: "Aland Islands" },
-  { key: "al", value: "al", flag: "al", text: "Albania" },
-  { key: "dz", value: "dz", flag: "dz", text: "Algeria" },
-  { key: "as", value: "as", flag: "as", text: "American Samoa" },
-  { key: "ad", value: "ad", flag: "ad", text: "Andorra" },
-  { key: "ao", value: "ao", flag: "ao", text: "Angola" },
-  { key: "ai", value: "ai", flag: "ai", text: "Anguilla" },
-  { key: "ag", value: "ag", flag: "ag", text: "Antigua" },
-  { key: "ar", value: "ar", flag: "ar", text: "Argentina" },
-  { key: "am", value: "am", flag: "am", text: "Armenia" },
-  { key: "aw", value: "aw", flag: "aw", text: "Aruba" },
-  { key: "au", value: "au", flag: "au", text: "Australia" },
-  { key: "at", value: "at", flag: "at", text: "Austria" },
-  { key: "az", value: "az", flag: "az", text: "Azerbaijan" },
-  { key: "bs", value: "bs", flag: "bs", text: "Bahamas" },
-  { key: "bh", value: "bh", flag: "bh", text: "Bahrain" },
-  { key: "bd", value: "bd", flag: "bd", text: "Bangladesh" },
-  { key: "bb", value: "bb", flag: "bb", text: "Barbados" },
-  { key: "by", value: "by", flag: "by", text: "Belarus" },
-  { key: "be", value: "be", flag: "be", text: "Belgium" },
-  { key: "bz", value: "bz", flag: "bz", text: "Belize" },
-  { key: "bj", value: "bj", flag: "bj", text: "Benin" },
+const products = [
+  { key: "cholecap", value: "cholecap", text: "Cholecap" },
+  { key: "zap", value: "zap", text: "Zap-a-Pain" },
+  { key: "nasalclear", value: "nasalclear", text: "Nasalclear" },
+  { key: "nova", value: "nova", text: "Nova-itch" },
 ];
 
 export default class Header extends Component {
@@ -39,15 +20,15 @@ export default class Header extends Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Drug Data</Nav.Link>
-            <Nav.Link href="#link">Sales Targets</Nav.Link>
+            <Nav.Link href="drug-data">Drug Data</Nav.Link>
+            <Nav.Link href="sales-targets">Sales Targets</Nav.Link>
           </Nav>
           <Dropdown
             placeholder="Select Drug"
             fluid
             search
             selection
-            options={countryOptions}
+            options={products}
             onChange={this.props.handleChange}
           />
         </Navbar.Collapse>
