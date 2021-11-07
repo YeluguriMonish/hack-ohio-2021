ab40d6a3a967a7e1370bcc132bf2f342bc2b5ee0
Jeff Valli <jefferysvalli@gmail.com>
2021-11-06 22:23:03 -0400

    Creat bargraph json and KV jsons

--- data_analytics/barchartjson.py
+++ data_analytics/barchartjson.py
@@ -1,9 +1,19 @@
 import json
 
-def create_bargraph_json(file):
+def create_bargraph_json(file, saveFile):
     with open(file) as f:
-        string = json.dump(f)
-        data = json.loads(string)
-    print(data)
+        data = json.load(f)
+    barGraphData = []
+    for item in data[0:5]:
+        dict = {}
+        dict["name"] = item["first_name"]+" "+item["last_name"]
+        dict["TRx"] = item["TRx_sum"]
+        dict["NRx"] = item["NRx_sum"]
+        barGraphData.append(dict)
+    with open(saveFile, 'w') as outFile:
+        json.dump(barGraphData, outFile)
 
-create_bargraph_json('data/topSellersCholecap.json')
+create_bargraph_json('data/topSellersCholecap.json', 'data/bargraphCholecap.json')
+create_bargraph_json('data/topSellersNasalclear.json', 'data/bargraphNasalclear.json')
+create_bargraph_json('data/topSellersNova_itch.json', 'data/bargraphNova_itch.json')
+create_bargraph_json('data/topSellersZap_a_Pain.json', 'data/bargraphZap_a_Pain.json')
